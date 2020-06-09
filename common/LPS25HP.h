52677edad02d7203f8dfc2a5e5151f5d563805c8
Wyver500 <65734248+Wyver500@users.noreply.github.com>
2020-06-09 22:23:25 +0200

    Add files via upload

---
+++ common/LPS25HP.h
@@ -0,0 +1,30 @@
+
+typedef struct LPS25HP{
+    double timestamp; // Zeitstempel in Sekunden (LINUX)
+
+    int Jahr;
+    int Monat;
+    int Tag;
+    int Stunde;
+    int Minute;
+    int Sekunde;
+
+    int TEMP_OUT_L;  // Temperatur LSB
+    int TEMP_OUT_H;  // Temperatur MSB
+    int TEMP;
+
+    int PRESS_OUT_XL;// Druck LSB       (hPa)
+    int PRESS_OUT_L; // Druck Mid Part
+    int PRESS_OUT_H; // Druck MSB
+    int PRESS;
+
+}LPS25HP;
+
+void print_struct_array(struct LPS25HP*, int);
+struct LPS25HP* read_LPS25HP(char*);
+void write_struct_array(struct LPS25HP*, char*, int);
+int count_Records(struct LPS25HP*);
+void UnixzeitNachDatum(unsigned long int,int*, int*, int*,int*, int*, int*);
+void arraytimecalc(struct LPS25HP*, int);
+void PRESS_CALC(struct LPS25HP*, int);
+void TEMP_CALC(struct LPS25HP*, int);
---
+++ common/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LPS25HP.h"
+
+
+int main()
+{
+    //Variablen zur Datendatei Einlesen
+    char filename_read[] = "Test_Data_45_LPS25_02.csv"; // Datei zum Einlesen
+
+    struct LPS25HP *All_Records;                         // Pointer auf Startadresse des Arrays
+    ////////////////////////////////////////////////
+
+    All_Records = read_LPS25HP(filename_read);           // Einlesen der Daten
+
+    int numb_Records = count_Records(All_Records);      // Z‰hlen der Records
+
+    arraytimecalc(All_Records, numb_Records);           //Umrechnung der Unix zeit in regul‰res Zeitmaﬂ
+
+    PRESS_CALC(All_Records, numb_Records);              //BIT in hPa
+
+    TEMP_CALC(All_Records, numb_Records);               //BIT in ∞C
+
+    //write_struct_array(All_Records, filename_write, numb_Records);   // Schreiben der Daten in neue Datei
+    //char filename_write[] = "write_test.txt";
+
+    print_struct_array(All_Records, numb_Records);      //Daten Ausgeben
+
+    free(All_Records);                                  // Speicher freigeben
+    int l=0;
+    scanf("%d",&l);
+    return 0;
+}
