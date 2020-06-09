b2c181f13eee8a0ceeeff2240ba3e57f4a58dc5c
Wyver500 <65734248+Wyver500@users.noreply.github.com>
2020-06-09 22:33:54 +0200

    Add files via upload

---
+++ common/common/CMakeFiles/LPS25HP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../lib/libLPS25HP.a"
+  "../lib/libLPS25HP.pdb"
+  "CMakeFiles/LPS25HP.dir/LPS25HP.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LPS25HP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
