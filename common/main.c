d95aadc4ad87b4c322cc00a9a30e81616bd2b4f1
Wyver500 <65734248+Wyver500@users.noreply.github.com>
2020-06-10 22:13:46 +0200

    Update main.c

--- common/main.c
+++ common/main.c
@@ -184,7 +184,7 @@ int main()
         //write_struct_array(All_Records, filename_write, numb_Records);   // Schreiben der Daten in neue Datei
         //char filename_write[] = "write_test.txt";
 
-        print_struct_array(All_Records, numb_Records);      //Daten Ausgeben
+        print_struct_array(All_Records, numb_Records,XXXXXXbeginn,XXXXXXXXXXXXend);      //Daten Ausgeben
 
         free(All_Records);                                  // Speicher freigeben
 
