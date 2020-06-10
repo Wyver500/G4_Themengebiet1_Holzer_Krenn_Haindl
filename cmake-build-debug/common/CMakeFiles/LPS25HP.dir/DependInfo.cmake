8847b7cc7af78ae39de70aee4c22e6f405a60c26
mr19b501 <65868537+mr19b501@users.noreply.github.com>
2020-06-10 17:12:33 +0200

    Ich glaub mit einer main gehts leichter

---
+++ cmake-build-debug/common/CMakeFiles/LPS25HP.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/clemens/Desktop/G4_Themengebiet1_Holzer_Krenn_Haindl/common/LPS25HP.c" "C:/Users/clemens/Desktop/G4_Themengebiet1_Holzer_Krenn_Haindl/cmake-build-debug/common/CMakeFiles/LPS25HP.dir/LPS25HP.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
