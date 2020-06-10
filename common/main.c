f263b04f88bc1f83f3c5b7acb5aacc51705f9873
mr19b501 <65868537+mr19b501@users.noreply.github.com>
2020-06-10 16:57:07 +0200

    Ich glaub mit einer main gehts leichter

--- common/main.c
+++ common/main.c
@@ -6,6 +6,58 @@
 
 int main()
 {
+
+    int Sensor;
+    char Sen1 []= "HTS221 (Luftfeuchtigkeit und Temperatur)";
+    char Sen2 []=  "LPS25HB (Druck)";
+//    char Sen3 []= "LSM9DS1 (XYZ-Koordinaten)";
+    int time_start;
+    int time_end;
+    printf("------------------------------\n");
+    printf("Alles Sensor Daten wurden am 15.04.2020 aufgezeichnet!\n");
+    printf("------------------------------\n");
+    printf("Welche Sensordaten sind gewuenscht?\n");
+    printf("Zur Auswahl stehen:\n");
+    printf("1. HTS221 (Luftfeuchtigkeit und Temperatur)\n");
+    printf("2. LPS25HB (Druck)\n");
+//    printf("3. LSM9DS1 (XYZ-Koordinaten)\n");
+    printf("------------------------------\n");
+    printf("Geben Sie gewuenschten Sensor ein:\n");
+    scanf("%d", &Sensor);
+    printf("------------------------------\n");
+    if (Sensor == 1){
+        printf("Gewaehlter Sensor ist:\n%d. %s\n", Sensor, Sen1);
+        printf("------------------------------\n");
+        printf("Geben Sie einen Zeitraum an in dem Sie die Sensordaten haben wollen\n");
+        printf("------------------------------\n");
+        printf("Gemessener Zeitraum 17:29:55 - 17:34:35\n");
+        printf("------------------------------\n");
+        printf("Anfang:");
+        scanf("%d", &time_start);
+        printf("------------------------------\n");
+        printf("Ende:");
+        scanf("%d", &time_end);
+        printf("------------------------------\n");
+
+    }else if(Sensor == 2){
+        printf("Gewaehlter Sensor ist:\n%d. %s\n", Sensor, Sen2);
+        printf("------------------------------\n");
+        printf("Geben Sie einen Zeitraum an in dem Sie die Sensordaten haben wollen\n");
+        printf("------------------------------\n");
+        printf("Gemessener Zeitraum 19:44:32 - 19:49:48\n");
+        printf("------------------------------\n");
+        printf("Anfang:");
+        scanf("%d", &time_start);
+        printf("------------------------------\n");
+        printf("Ende:");
+        scanf("%d", &time_end);
+        printf("------------------------------\n");
+    }else if(Sensor == 3){
+        printf("Gewaehlter Sensor ist:\n%d. %s\n", Sensor, Sen2);
+    }
+
+
+
     //Variablen zur Datendatei Einlesen
     char filename_read[] = "Test_Data_45_LPS25_02.csv"; // Datei zum Einlesen
 
@@ -22,8 +74,8 @@ int main()
 
     TEMP_CALC(All_Records, numb_Records);               //BIT in °C
 
-    //char filename_write[] = "write_test.txt";
     //write_struct_array(All_Records, filename_write, numb_Records);   // Schreiben der Daten in neue Datei
+    //char filename_write[] = "write_test.txt";
 
     print_struct_array(All_Records, numb_Records);      //Daten Ausgeben
 
